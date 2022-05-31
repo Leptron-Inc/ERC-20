e608fd1345f486ac7d75b6fb7842f731a89346b8
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2022-05-31 18:53:21 +0530

    Update ERC20.sol

--- ERC20/ERC20.sol
+++ ERC20/ERC20.sol
@@ -2,7 +2,7 @@
 // OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/ERC20.sol)
 
 pragma solidity ^0.8.0;
-
+ 
 import "./IERC20.sol";
 import "./extensions/IERC20Metadata.sol";
 import "../../utils/Context.sol";
